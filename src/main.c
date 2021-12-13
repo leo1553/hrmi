d37783b61f65c5aa45fe2f63a4a53ce658cbf52b
leo1553 <leonardoleal.qm@hotmail.com.br>
2021-12-12 23:53:10 -0300

    Removing generated file

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include "parse.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc < 1) {
         fprintf(stderr, "Usage: %s [file]\n", argv[0]);
         return 1;
     }
